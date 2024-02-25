28aa48a2902a5618a3fd769e80c5e4752c9823eb
Cynastic <111385987+Cynastic@users.noreply.github.com>
2024-02-25 22:47:52 +0100

    Cleanup

--- stuff.js
+++ stuff.js
@@ -1,39 +1,18 @@
-let h = 0;
-let currentText = 'this will be interesting.';
+let score = 0;
 let lines = [];
 
 fetch('https://cynastic.github.io/StarWarsEpisode4.txt')
-  .then(response => response.text())
-  .then(text => {
-    // Log the text to the console
-    lines = text.split('\n').filter(line => line !== "");
-  })
-  .catch(error => {
-    // Handle the error
-    console.error(error);
-  });
+.then(response => response.text())
+.then(text => {
+  lines = text.split('\n').filter(line => line !== "");
+})
+.catch(error => {
+  console.error(error);
+});
 
-function doSomething()
+function getNextLine()
 {
-    h += 1;
-    document.getElementById('score').innerHTML = h;
-
-    document.getElementById('levelText').innerHTML = lines[h];
-}
-
-function revealLevel(currentLevel)
-{
-    switch(currentLevel)
-    {
-        case 1:
-            currentText = 'Wow, You Started';
-        break;
-        case 2:
-            currentText = 'Hello There.';
-        break;
-        case 3:
-            currentText = 'General Kenobi.';
-        break;
-    }
-    document.getElementById('levelText').innerHTML = currentText;
+  score++;
+  document.getElementById('score').innerHTML = score;
+  document.getElementById('levelText').innerHTML = lines[score];
 }
